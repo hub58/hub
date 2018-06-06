a60978dec6bbb68399807ebd0d2e0803f1a9e782
Sandip Patel <srpatel19590@gmail.com>
2018-06-06 23:17:27 +0530

    Change symbol, address prefix and initminer public key Fixed cli wallet account creation issue

--- libraries/protocol/include/steemit/protocol/config.hpp
+++ libraries/protocol/include/steemit/protocol/config.hpp
@@ -40,14 +40,14 @@
 #define STEEMIT_OWNER_AUTH_HISTORY_TRACKING_START_BLOCK_NUM 1
 #else // IS LIVE STEEM NETWORK
 
-#define STEEMIT_INIT_PUBLIC_KEY_STR             "STM8GC13uCZbP44HzMLV6zPZGwVQ8Nt4Kji8PapsPiNq1BK153XTX"
+#define STEEMIT_INIT_PUBLIC_KEY_STR             "HUB7K1q9F2orgnHM42GqVscVm7VpY8RpdPZZJfdbfhzmA1BFQXX8Z"
 #define STEEMIT_CHAIN_ID                        (steemit::protocol::chain_id_type())
 #define VESTS_SYMBOL  (uint64_t(6) | (uint64_t('V') << 8) | (uint64_t('E') << 16) | (uint64_t('S') << 24) | (uint64_t('T') << 32) | (uint64_t('S') << 40)) ///< VESTS with 6 digits of precision
-#define STEEM_SYMBOL  (uint64_t(3) | (uint64_t('S') << 8) | (uint64_t('T') << 16) | (uint64_t('E') << 24) | (uint64_t('E') << 32) | (uint64_t('M') << 40)) ///< STEEM with 3 digits of precision
-#define SBD_SYMBOL    (uint64_t(3) | (uint64_t('S') << 8) | (uint64_t('B') << 16) | (uint64_t('D') << 24) ) ///< STEEM Backed Dollars with 3 digits of precision
+#define STEEM_SYMBOL  (uint64_t(3) | (uint64_t('H') << 8) | (uint64_t('U') << 16) | (uint64_t('B') << 24) | (uint64_t('5') << 32) | (uint64_t('8') << 40)) ///< STEEM with 3 digits of precision
+#define SBD_SYMBOL    (uint64_t(3) | (uint64_t('H') << 8) | (uint64_t('B') << 16) | (uint64_t('D') << 24) ) ///< STEEM Backed Dollars with 3 digits of precision
 #define STMD_SYMBOL   (uint64_t(3) | (uint64_t('S') << 8) | (uint64_t('T') << 16) | (uint64_t('M') << 24) | (uint64_t('D') << 32) ) ///< STEEM Dollars with 3 digits of precision
-#define STEEMIT_SYMBOL                          "STEEM"
-#define STEEMIT_ADDRESS_PREFIX                  "STM"
+#define STEEMIT_SYMBOL                          "HUB58"
+#define STEEMIT_ADDRESS_PREFIX                  "HUB"
 
 #define STEEMIT_GENESIS_TIME                    (fc::time_point_sec(1458835200))
 #define STEEMIT_MINING_TIME                     (fc::time_point_sec(1458838800))
